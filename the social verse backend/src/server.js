9f3c367b7f6a42ff572d9024af6afd6652be18bc
Barry-White17 <whitwb2@gmail.com>
2026-09-24 12:36:10 +0100

    Comment out socket.io related code plus httpServer

--- the social verse backend/src/server.js
+++ the social verse backend/src/server.js
@@ -6,17 +6,11 @@ import mongoose from "mongoose";
 import { v4 as uuidv4 } from "uuid";
 import { v7 as uuidv7 } from "uuid";
 import { database } from "./models/database.js";
-import http from "http";
-import { Server } from "socket.io";
+// import http from "http";
+// import { Server } from "socket.io";
 
 // Connection URL
-if (process.env.ENVIRONMENT == "sandbox") {
-  await database(process.env.DEV_DATABASE_URL);
-}
-
-if (process.env.ENVIRONMENT == "test") {
-  await database(process.env.TEST_DATABASE_URL);
-}
+await database(process.env.DATABASE_URL);
 
 process.env.JWT_SECRET = uuidv4();
 
@@ -26,5 +20,5 @@ app.listen(process.env.PORT, (err) => {
   }
   console.info("Server started on port %s.", process.env.PORT);
 });
-const server = http.createServer(app);
-const io = new Server(server);
+// const server = http.createServer(app);
+// const io = new Server(server);
